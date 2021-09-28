50aa4efd59693f3ab5353b6a1a2ec59000260f31
nlenno1 <nick.lennon@hotmail.co.uk>
2021-09-28 15:16:13 +0000

    completed mysql tutorial

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,19 +1,110 @@
 import os
 import pymysql
 
-# Get username
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
-#Connect to database
-connection = pymysql.connect(host = 'localhost', user=username, password='', db='Chinook')
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
 
+# insert one row
+"""
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist LIMIT 10;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        row = ("bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
 finally:
-    # Close the connection to SQL regardles of whether the above was successful
+    connection.close
+"""
+
+# inserting many rows
+"""
+try:
+    with connection.cursor() as cursor:
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 13:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        connection.commit()
+finally:
     connection.close()
+"""
+# update 
+"""
+try:
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'Bob';")
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# update with a tuple
+"""
+try:
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;", (23, 'Bob'))
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# update many rows
+"""
+try:
+    with connection.cursor() as cursor:
+        rows = [(23, 'bob'), (24, 'jim'), (25, 'fred')]
+        cursor.executemany("UPDATE Friends SET age = %s WHERE name = %s;", rows)
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# delete row
+"""
+try:
+    with connection.cursor() as cursor:
+        cursor.execute("DELETE FROM Friends WHERE name = 'Bob';")
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# alternative delete using string interpolation
+"""
+try:
+    with connection.cursor() as cursor:
+        rows = cursor.execute("DELETE FROM Friends WHERE name = %s;", 'bob')
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# delete many
+"""
+try:
+    with connection.cursor() as cursor:
+        rows = cursor.executemany("DELETE FROM Friends WHERE name = %s;", ['bob', 'jim'])
+        connection.commit()
+finally:
+    connection.close()
+"""
+
+# delete WHERE IN
+"""
+try:
+    with connection.cursor() as cursor:
+        list_of_names = ['fred', 'Fred']
+        #Prepare a string with the same number of placeholders as on list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit()
+finally:
+    connection.close()
+"""
+
